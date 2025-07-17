491e234e869772fbc53e8c65c0fedbc49f692c42
Nguyen Trong Nghia (FPL CT) <ntnghia.it01@gmail.com>
2025-07-17 11:21:20 +0700

    Initial commit

---
+++ src/main/java/com/poly/duan/java/DuAn1PTPMJava.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.poly.duan.java;
+
+import com.poly.duan.java.views.LoginFrame;
+
+/**
+ *
+ * @author macbookprom1
+ */
+public class DuAn1PTPMJava {
+
+    public static void main(String[] args) {
+        
+//        Viết thêm 1 hàm kiểm tra đã có tài khoản
+//       Với quyền admin cao nhất trong db chưa
+//  Nếu chưa có thì thực hiện tạo cho hệ thống 1 tài khoản
+
+        LoginFrame loginFrame = new LoginFrame();
+        loginFrame.setVisible(true);
+    }
+}
---
+++ src/main/java/com/poly/duan/java/utils/Constants.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.poly.duan.java.utils;
+
+/**
+ *
+ * @author macbookprom1
+ */
+public class Constants {
+//    Tạo biến là hằng số 
+    public static final String HOME_PANEL = "HOME_PANEL";
+}
